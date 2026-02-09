2abc39e93d2c270f3ed39cd95407baeaa53c6a87
Jadden Picardal <jaddenki@gmail.com>
2026-02-09 11:43:34 -0500

    misc optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,35 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.jaddens.page',
 	integrations: [mdx(), sitemap()],
+	prefetch: true,
+	image: {
+		remotePatterns: [
+			{ protocol: 'https', hostname: 'github.com', pathname: '/**' },
+			{ protocol: 'https', hostname: 'www.google.com', pathname: '/**' },
+		],
+	},
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.local(),
+				name: 'GeistPixel',
+				cssVariable: '--font-geist-pixel',
+				options: {
+					variants: [
+						{
+							weight: 400,
+							style: 'normal',
+							src: ['./src/assets/fonts/GeistPixel-Square.woff2'],
+						},
+					],
+				},
+			},
+		],
+	},
 });
