88a19562a15ae2021d4cae498a852f7cc3dc049c
Jadden Picardal <jaddenki@gmail.com>
2026-02-11 10:38:24 -0500

    make my name uppercase too lol

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "jadden picardal";
+export const SITE_TITLE = "Jadden Picardal";
 export const SITE_DESCRIPTION =
   "Jadden Picardal is a programmer and designer, currently working at lowercase. This is a selection of her work and thoughts.";
